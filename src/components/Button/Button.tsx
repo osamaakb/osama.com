a5abb7d953ae648877267259189028ef1a9908ec
osamaakb <osama98akb@gmail.com>
2021-06-14 16:01:29 +0300

    Fix(Style): updated button hover style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,11 +8,11 @@ export interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ children, light, handleClick }) => {
   const color = light
-    ? "border-white text-white"
-    : "border-primary text-primary";
+    ? "border-white text-white hover:bg-white hover:text-primary"
+    : "border-primary text-primary hover:bg-primary hover:text-white";
   return (
     <button
-      className={`${color} border-2 bg-transparent px-5 py-2 text-xl ring-transparent m-2 hover:bg-white hover:text-primary duration-500`}
+      className={`${color} border-2 bg-transparent px-5 py-2 text-xl ring-transparent m-2 duration-500`}
       onClick={handleClick}
     >
       {children}
