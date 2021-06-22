ce519dccdc3f1cfe334e10e890c70d5e43a700de
osamaakb <osama98akb@gmail.com>
2021-06-22 14:14:35 +0300

    Fix: made email input required

--- src/components/NewsLetter/NewsLetter.tsx
+++ src/components/NewsLetter/NewsLetter.tsx
@@ -9,21 +9,25 @@ const NewsLetter: React.FC<NewsLetterProps> = () => {
       <div className="flex justify-center items-center">
         <h1 className="text-4xl pt-12">Susbscribe to my newsletter</h1>
       </div>
-      <div className="pt-10 pb-14 flex justify-center">
-        <input
-          className="border-primary border h-12 w-80 outline-none p-3"
-          height={200}
-          placeholder="Enter Address"
-          value={emailInputValue}
-          onChange={(e) => setEmailInputValue(e.target.value)}
-        />
-        <button
-          className="bg-primary text-white mx-2 h-12 px-4 hover:bg-white hover:text-primary
+      <form>
+        <div className="pt-10 pb-14 flex justify-center">
+          <input
+            className="border-primary border h-12 w-80 outline-none p-3"
+            height={200}
+            placeholder="Enter Address"
+            value={emailInputValue}
+            onChange={(e) => setEmailInputValue(e.target.value)}
+            required
+          />
+          <button
+            type="submit"
+            className="bg-primary text-white mx-2 h-12 px-4 hover:bg-white hover:text-primary
           border border-primary  duration-500"
-        >
-          Subscribe
-        </button>
-      </div>
+          >
+            Subscribe
+          </button>
+        </div>
+      </form>
       <span className="pb-5 text-gray-500 text-sm">
         You will not receive any spam from me.
       </span>
