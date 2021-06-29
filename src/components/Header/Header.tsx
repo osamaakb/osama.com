252a9b35025d8c675f56b4727d2c4d102bfa77fe
osamaakb <osama98akb@gmail.com>
2021-06-29 13:58:50 +0300

    Style: header centeralization

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,6 @@ import DownArrowIcon from "../../Icons/DownArrow";
 import Button from "../Button/Button";
 import SocialIcons from "../SocialIcons";
 import { useRouter } from "next/router";
-import { route } from "next/dist/next-server/server/router";
 
 export interface HeaderProps {}
 
@@ -23,12 +22,16 @@ const Header: React.FC<HeaderProps> = () => {
         />
         <div className="h-screen w-screen absolute bg-primary opacity-40"></div>
         <div className="text-white h-screen lg:w-2/4 md:w-2/4 sm:w-full w-full z-10 absolute right-0 p-4 lg:p-0 md:p-0 sm:p-4">
-          <div className="flex flex-col h-full justify-center content-end">
-            <h1 className="text-left ml-2 text-3xl lg:mb-4 md:mb-4 sm:mb-8 mb-8 leading-snug">
-              <b>Every coder has their journey.</b> <br />
-              <span className="text-3xl">This is mine.</span>
+          <div className="flex flex-col h-full justify-center px-2">
+            <h1 className="lg:text-left md:text-left sm:text-center text-center lg:ml-2 md:ml-2 sm:ml-0 ml-0 text-3xl lg:mb-4 md:mb-4 sm:mb-4 mb-4 leading-snug lg:mr-28 md:mr-20 sm:mr-0 mr-0">
+              <b>
+                I teach code and make programs that aim to make people’s life
+                better.
+              </b>
+              <br />
+              {/* <span className="text-3xl">This is mine.</span> */}
             </h1>
-            <div className="flex flex-row">
+            <div className="flex flex-row justify-center">
               {NAV_ITEMS.map(({ title, path }, itemIdx) => {
                 return (
                   <Button
