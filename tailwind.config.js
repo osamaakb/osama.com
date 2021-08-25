5e1017f8f96b4c206170dd996cbdd8216ba81c57
osamaakb <osama98akb@gmail.com>
2021-08-25 13:59:35 +0300

    add heights to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
       xl: '1440px',
     },
     extend: {
+      height: {
+        "screen-1/2": "50vh",
+        "screen-1/3": "33vh",
+        "screen-1/4": "25vh",
+        "screen-3/4": "75vh",
+      },
       fontFamily: {
         "sans": [
           "Mulish",
